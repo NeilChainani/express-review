af403691f27551324d33bcf0d1b099e86e0558ed
Neil Chainani <Neil.Chainani@lexisnexisrisk.com>
2018-11-08 17:54:07 -0500

    Playing around with express
    wee

---
+++ bird.js
@@ -0,0 +1,3 @@
+var express = require("express");
+var router = express.Router(); //so instead of calling express to export it you call router
+modle.exports = router;
--- index.js
+++ index.js
@@ -12,6 +12,27 @@ app.use(requestTime);
 app.get("/", (req, res) => {
   res.send(`The time is now ${req.requestTime}`);
 });
+var cb0 = (req, res, next) => {
+  console.log("trial 1");
+  next();
+};
+var cb1 = (req, res, next) => {
+  console.log("trial 2");
+  next();
+};
+
+app.get(
+  "/me/:id",
+  [cb0, cb1],
+  (req, res, next) => {
+    res.send(req.params.id);
+    console.log("trial 3");
+    next();
+  },
+  (req, res) => {
+    console.log("trial 4");
+  }
+);
 
 var port = process.env.port || 3000;
 app.listen(port);
