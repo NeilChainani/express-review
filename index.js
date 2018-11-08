2dbf05c068343b537e0552a4b87bbc78eb5b0cb4
Neil Chainani <Neil.Chainani@lexisnexisrisk.com>
2018-11-08 16:25:42 -0500

    Get Date time using middlware in express
    Get Date time using middlware in express

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
-var app = express(); //
+var app = express();
+var log = (req, res, next) => {
+  console.log("logging worked"); //add in the use function if you want to add a logger.
+  next();
+};
+var requestTime = (req, res, next) => {
+  req.requestTime = Date.now();
+  next();
+};
+app.use(requestTime);
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send(`The time is now ${req.requestTime}`);
 });
-app.get("/test", (req, res) => {
-  res.send([3, 2, 1]);
-});
+
 var port = process.env.port || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port} `);
-});
+app.listen(port);
+console.log(`listening to port ${port}`);
